f62279bd3f444630865fe0cc915f224fa32f9f84
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-03-31 10:12:45 -0500

    Timeouts and try catch trial

--- lib/api/queries.dart
+++ lib/api/queries.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:royal_reader/types/book.dart';
@@ -7,20 +9,28 @@ import 'package:royal_reader/types/chapter.dart';
 import '../types/chapterNameId.dart';
 
 const url = "http://192.168.1.238:8080";
+const timeout = Duration(seconds: 2);
 
 Future<List<Book>> fetchBooks() async {
   List<Book> books = [];
-  var res = await http.get(Uri.parse('$url/books'));
-  for (var jsonBook in jsonDecode(res.body)){
-    books.add(Book.fromJson(jsonBook));
+  try{
+    var res = await http.get(Uri.parse('$url/books')).timeout(timeout);
+    for (var jsonBook in jsonDecode(res.body)){
+      books.add(Book.fromJson(jsonBook));
+    }
+  } on TimeoutException catch (_) {
+    // A timeout occurred.
+  } on SocketException catch (_) {
+    // Other exception
   }
+
 
   return books;
 }
 
 Future<List<ChapterNameId>> fetchChapterNamesAndIds(int id) async {
   List<ChapterNameId> chapters = [];
-  var res = await http.get(Uri.parse('$url/chapter/all/nameId/$id'));
+  var res = await http.get(Uri.parse('$url/chapter/all/nameId/$id')).timeout(timeout);
   for (var jsonChapter in jsonDecode(res.body)){
     chapters.add(ChapterNameId.fromJson(jsonChapter));
   }
@@ -29,7 +39,7 @@ Future<List<ChapterNameId>> fetchChapterNamesAndIds(int id) async {
 }
 
 Future<Chapter> fetchChapter(int id) async {
-  var res = await http.get(Uri.parse('$url/chapter/$id'));
+  var res = await http.get(Uri.parse('$url/chapter/$id')).timeout(timeout);
   var jsonChapter = jsonDecode(res.body);
 
   return Chapter.fromJson(jsonChapter);
