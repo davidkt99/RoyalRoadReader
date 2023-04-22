08e582f201a6ca30f56bd341ab156aa5709f978a
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-04-22 18:18:32 -0500

    AddBook loading states finished

--- lib/addBook/add_book_dialog.dart
+++ lib/addBook/add_book_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:royal_reader/addBook/add_book_bloc.dart';
 
 import '../api/mutations.dart';
 
@@ -12,33 +14,70 @@ class AddBookDialog extends StatefulWidget {
 class _AddBookDialogState extends State<AddBookDialog> {
   late String textFieldVal;
 
-  Future<void> handleSubmit(BuildContext context, String url) async {
-    debugPrint(textFieldVal);
-    await addBook(textFieldVal)
-        .whenComplete(() => (){
-      Navigator.of(context).pop();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Add Book'),
-      content: SingleChildScrollView(
-        child: TextField(
-          onChanged: (val){
-            textFieldVal = val;
-          },
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () {
-            handleSubmit(context, textFieldVal);
-          },
-          child: const Text('Submit'),
-        ),
-      ],
+    return BlocProvider(
+      create: (context) => AddBookBloc(),
+      child: BlocBuilder<AddBookBloc, AddBookState>(
+        builder: (context, state) {
+          if(state is AddBookInitialState){
+            return AlertDialog(
+              title: const Text('Add Book'),
+              content: SingleChildScrollView(
+                child: TextField(
+                  onChanged: (val) {
+                    textFieldVal = val;
+                  },
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    final bloc = BlocProvider.of<AddBookBloc>(context);
+                    bloc.add(AddBookEvent(url: textFieldVal));
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            );
+          }
+          if(state is AddBookLoadingState){
+            return const AlertDialog(
+              title: Text('This may take sometime'),
+              content: SingleChildScrollView(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          }
+          if (state is AddBookErrorState) {
+            return AlertDialog(
+              title: const Text('Add Book'),
+              content: SingleChildScrollView(
+                child: Center(
+                  child: Text(
+                    state.errorMessage,
+                  ),
+                ),
+              ),
+            );
+          }
+          if (state is AddBookSuccessState) {
+            return const AlertDialog(
+              title: Text('Add Book'),
+              content: SingleChildScrollView(
+                child: Center(
+                  child: Text(
+                    "Book Successfully Added!",
+                  ),
+                ),
+              ),
+            );
+          }
+
+          return Container();
+        }),
     );
   }
 }
