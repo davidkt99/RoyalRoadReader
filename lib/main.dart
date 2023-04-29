27a504e05aea8d85f645bb58626a1fa162c99457
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-04-29 13:18:16 -0500

    Added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
+          darkTheme: ThemeData.dark(),
           routerConfig: router,
         );
       }
