23f17fad72d6a91bcb3865a8cfcf997b27edc944
David Thompson <49345822+davidkt99@users.noreply.github.com>
2023-05-05 19:36:31 -0500

    Fix Book model for new book class

--- lib/booksList/book.dart
+++ lib/booksList/book.dart
@@ -3,13 +3,15 @@ import 'package:royal_reader/util/parsers.dart';
 class Book {
   final int id;
   final String? name;
-  final String url;
+  final String author;
+  final String imageUrl;
   final int numOfChap;
 
   const Book({
     required this.id,
     required this.name,
-    required this.url,
+    required this.author,
+    required this.imageUrl,
     required this.numOfChap
   });
 
@@ -17,8 +19,9 @@ class Book {
     return Book(
       id: json['id'],
       name: parseHtmlString(json['name']),
-      url: json['url'],
-        numOfChap: json['numOfChap']
+        author: json['author'],
+        imageUrl: json['imageUrl'],
+        numOfChap: json['numOfChap'],
     );
   }
 }
